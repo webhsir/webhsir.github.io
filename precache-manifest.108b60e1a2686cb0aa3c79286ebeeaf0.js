8e4d626ac07c8f151cb6c53570db04128ed1bb84
e <257644729@qq.com>
2020-09-29 16:38:00 +0800

    提交

---
+++ precache-manifest.108b60e1a2686cb0aa3c79286ebeeaf0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da15a2f5744ddff8a72e",
+    "url": "/css/app.83ee27e2.css"
+  },
+  {
+    "revision": "935f32ca09e09a253e465631cf047ae3",
+    "url": "/img/1.935f32ca.png"
+  },
+  {
+    "revision": "717616b59765dbcb23169753b527984b",
+    "url": "/img/2.717616b5.png"
+  },
+  {
+    "revision": "13b6c31fd301bff6ff8a4bfeb7fc77ea",
+    "url": "/img/3.13b6c31f.png"
+  },
+  {
+    "revision": "818483d7b29552cfc1043c17b5dd41f2",
+    "url": "/img/4.818483d7.png"
+  },
+  {
+    "revision": "ef621dc7d36904a3c434b32ceddd2d6b",
+    "url": "/img/5.ef621dc7.png"
+  },
+  {
+    "revision": "ad8f0beab2aaaddd755bfcebe838d87a",
+    "url": "/img/6.ad8f0bea.png"
+  },
+  {
+    "revision": "6c77f52debd1084d1ed34d9e93546104",
+    "url": "/img/people.6c77f52d.png"
+  },
+  {
+    "revision": "3e9603184cf8ad2feb63f47d46beb741",
+    "url": "/img/peopleC.3e960318.png"
+  },
+  {
+    "revision": "421db1d3ffba2582ed2dacd3684b3de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da15a2f5744ddff8a72e",
+    "url": "/js/app.3150c6ac.js"
+  },
+  {
+    "revision": "64213485790498edca94",
+    "url": "/js/chunk-vendors.be9c61fa.js"
+  },
+  {
+    "revision": "5be09dfed0bda1759b2f8120600fb142",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
